dc37dcd5b00a78e0be916d7d49022f1bca443a74
JustMe17Artem <sematkinartem6@gmail.com>
2022-05-03 13:24:51 +0300

    working version

---
+++ WpfLeisure/Core/Classes_Core/DB_Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.ado;
+
+namespace Core.Classes_Core
+{
+    public class DB_Connection
+    {
+        public static LEISURE_KursachEntities connection = new LEISURE_KursachEntities();
+    }
+}
---
+++ WpfLeisure/Core/Classes_Core/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.ado;
+using System.Collections.ObjectModel;
+
+namespace Core.Classes_Core
+{
+    public class DataAccess
+    {
+        private static ObservableCollection<Role> roles = new ObservableCollection<Role>(DB_Connection.connection.Role);
+        public static ObservableCollection<Role> GetRoles()
+        {
+            return roles;
+        }
+    }
+}
